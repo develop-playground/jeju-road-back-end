cd1c839a55bd27ec7b0839b7daa86b493f84bacf
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-11-03 21:03:16 +0900

    #1 기본 프로젝트 재설정

---
+++ src/main/java/demo/jejuroadbackend/JejuRoadBackEndApplication.java
@@ -0,0 +1,13 @@
+package demo.jejuroadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuRoadBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JejuRoadBackEndApplication.class, args);
+  }
+
+}
