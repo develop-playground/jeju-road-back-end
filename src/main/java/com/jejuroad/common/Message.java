95fa1cb73f7a41af68114f89dd7c69ae27faff38
Cheolho Jeon <cheolho@edu.hanbat.ac.kr>
2022-01-08 13:47:20 +0900

    #16 @RestControllerAdvice를 활용한 예외처리 로직 구현

--- src/main/java/com/jejuroad/common/Message.java
+++ src/main/java/com/jejuroad/common/Message.java
@@ -8,9 +8,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum Message {
     COMMON_RESPONSE_OK(
-            HttpStatus.OK,
-            "COMMON-RES001",
-            "요청이 성공적으로 수행되었습니다."
+        HttpStatus.OK,
+        "COMMON-RES001",
+        "요청이 성공적으로 수행되었습니다."
+    ),
+    RESTAURANT_RESPONSE_NONE_CATEGORY(
+        HttpStatus.BAD_REQUEST,
+        "RESTAURANT-RES001",
+        "존재하지 않는 맛집 카테고리입니다."
     );
 
     private final HttpStatus httpStatus;
--- src/main/java/com/jejuroad/common/mapper/CategoryMapper.java
+++ src/main/java/com/jejuroad/common/mapper/CategoryMapper.java
@@ -1,10 +1,14 @@
 package com.jejuroad.common.mapper;
 
+import com.jejuroad.common.BusinessException;
+import com.jejuroad.common.Message;
 import com.jejuroad.domain.Category;
 import com.jejuroad.repository.CategoryRepository;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.jejuroad.common.Message.RESTAURANT_RESPONSE_NONE_CATEGORY;
 
 @Mapper(componentModel = "spring", uses = CategoryRepository.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public abstract class CategoryMapper {
@@ -12,7 +16,9 @@ public abstract class CategoryMapper {
     private CategoryRepository categoryRepository;
 
     public Category mapToCategoryFrom(String categoryName) {
-        return categoryRepository.findByName(categoryName);
+        return categoryRepository
+            .findByName(categoryName)
+            .orElseThrow(() -> new BusinessException(RESTAURANT_RESPONSE_NONE_CATEGORY));
     }
 
     public String mapToStringFrom(Category category) {
